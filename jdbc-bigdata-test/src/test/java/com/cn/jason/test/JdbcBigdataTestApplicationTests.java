bf4b7eb273221d312b2b29a512d2f0f2aa7ad62a
Administrator <15874053527@163.com>
2024-10-07 13:37:44 +0800

    bigdata新增抽取、导出逻辑

---
+++ jdbc-bigdata-test/src/test/java/com/cn/jason/test/JdbcBigdataTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.jason.test;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JdbcBigdataTestApplicationTests {
+
+	@Test
+    public void contextLoads() {
+	}
+
+}
